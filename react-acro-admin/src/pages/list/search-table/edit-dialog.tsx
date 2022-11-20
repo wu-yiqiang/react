52e7b5785d30c2f4c1dff67d1b58a49e5192a1f1
wu-yiqiang <wu_yiqiang@outlook.com>
2022-11-20 21:30:58 +0800

    修改edit组件

--- react-acro-admin/src/pages/list/search-table/edit-dialog.tsx
+++ react-acro-admin/src/pages/list/search-table/edit-dialog.tsx
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { Component } from 'react';
 import { Modal } from '@arco-design/web-react';
 import useLocale from '@/utils/useLocale';
 import locale from './locale';
 
-
-class Dialog extends React.Component {
+class Dialog extends Component {
   constructor(props) {
     super(props);
-    const [visible, setVisible] = useState(props.show);
     const t = useLocale(locale);
+    this.state = {
+      visible: this.props.show,
+      title: t['editDialog.title'],
+    };
   }
-
+  closed() {
+    this.setState({
+      visible: false,
+    });
+  }
   render() {
     return (
       <Modal
-      title={t['editDialog.title']}
-      visible={visible}
-      onOk={() => setVisible(false)}
-      onCancel={() => setVisible(false)}
-      autoFocus={false}
-      focusLock={true}
-    >
-      <p>
-        You can customize modal body text by the current situation. This modal
-        will be closed immediately once you press the OK button.
-      </p>
-    </Modal>
+        title={this.state.title}
+        visible={this.state.visible}
+        onOk={() => {
+          closed;
+        }}
+        onCancel={() => {
+          closed;
+        }}
+        autoFocus={false}
+        focusLock={true}
+      >
+        <p>
+          You can customize modal body text by the current situation. This modal
+          will be closed immediately once you press the OK button.
+        </p>
+      </Modal>
     );
   }
 }
