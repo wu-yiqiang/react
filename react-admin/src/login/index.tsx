d5304e636276b1aad2566e4e369e45202e5c4948
wu-yiqiang <wu_yiqiang@outlook.com>
2023-11-20 09:43:46 +0800

    优化登录UI

--- react-admin/src/login/index.tsx
+++ react-admin/src/login/index.tsx
@@ -14,19 +14,22 @@ function Login() {
     <div className="Login">
       <div className="card">
         <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
-          <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+          <Form.Item label="用户名" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
             <Input />
           </Form.Item>
 
-          <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+          <Form.Item label="密码" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
             <Input.Password />
           </Form.Item>
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
         </Form>
+        <div className='submit'>
+          <Button type="primary" htmlType="submit">
+            注册
+          </Button>
+          <Button type="primary" htmlType="submit">
+            登录
+          </Button>
+        </div>
       </div>
     </div>
   )
