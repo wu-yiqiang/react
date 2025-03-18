17c55f150bad6bbd8beeaf1eba36badf17bfba0f
wu-yiqiang <wu_yiqiang@outlook.com>
2025-03-19 00:37:58 +0800

    添加权限列表和角色列表

--- react-admin/src/pages/settings/permissions/index.tsx
+++ react-admin/src/pages/settings/permissions/index.tsx
@@ -1,3 +1,103 @@
+import Tabular from '@/components/Tabular.tsx'
+import { getTruckLists, getUsersLists } from '@/api/settings'
+import { useState } from 'react'
+import UserAddDialog from './permission-add-dialog'
+import './permission-manager.scss'
+import { Button, message } from 'antd'
 export default function PermissionManager() {
-  return '权限管理'
+  const [lists, setLists] = useState()
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [pager, setPager] = useState({
+    total: 0,
+    pageNo: 1,
+    pageSize: 10
+  })
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'displayName',
+      key: 'displayName'
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'email',
+      key: 'email'
+    },
+    {
+      title: '性别',
+      dataIndex: 'gender',
+      key: 'gender'
+    },
+    {
+      title: '部门',
+      dataIndex: 'department',
+      key: 'department'
+    },
+    {
+      title: '岗位',
+      dataIndex: 'positon',
+      key: 'positon'
+    },
+    {
+      title: '角色',
+      dataIndex: 'roles',
+      key: 'roles'
+    }
+  ]
+  const searchOptions = [{ name: 'keyword', label: '搜索', type: 'input' }]
+  const queryData = {
+    keyword: '',
+    gender: 'male'
+  }
+
+  const handleSearch = async (values: object) => {
+    const params = { ...values, ...queryData }
+    const { data } = await getTruckLists(params)
+    setLists(data.lists)
+    const datas = {
+      pageSize: data.pageSize,
+      total: data.total,
+      pageNo: data.pageNo
+    }
+    setPager(datas)
+  }
+  const formState = {
+    name: '',
+    email: '',
+    picture: ''
+  }
+  const handleNew = () => {
+    setDialogOpen(true)
+  }
+
+  const handleClose = () => {
+    setDialogOpen(false)
+  }
+
+  const handleOk = async (values: any) => {
+    const datas = { ...values, type: 1 }
+    await getUsersLists(datas)
+    message.success('操作成功')
+    setDialogOpen(false)
+  }
+  return (
+    <>
+      <Tabular
+        dataSource={lists}
+        total={pager.total}
+        pageNo={pager.pageNo}
+        pageSize={pager.pageSize}
+        columns={columns}
+        data={queryData}
+        searchOptions={searchOptions}
+        handleSearch={handleSearch}
+        right={
+          <Button type="primary" onClick={handleNew}>
+            新增
+          </Button>
+        }
+      ></Tabular>
+      <UserAddDialog open={dialogOpen} formState={formState} handleClose={handleClose} handleOk={handleOk} />
+    </>
+  )
 }
---
+++ react-admin/src/pages/settings/roles/role-add-dialog.tsx
@@ -0,0 +1,138 @@
+import { Form, Input, Modal } from 'antd'
+import { useEffect, useState } from 'react'
+import { postUser, getContractorDetail } from '@/api/settings'
+import {AES_ECB_ENCRYPT} from '@/utils/encrypt'
+import { isEmpty } from 'lodash-es'
+import { message, Select, Row, Col } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+class Contractor {
+  name: string
+  email: string
+  // gender: number
+  picture: string
+  password: string
+  constructor() {
+    this.name = ''
+    this.email = ''
+    this.password = ''
+    // this.gender = 0
+    this.picture = ''
+  }
+}
+export default function UserAddDialog(props: any) {
+  const { open, handleClose, target, handleOk } = props
+  const [editStatus, setEditStatus] = useState(false)
+  const [title, setTitle] = useState('新增')
+  const [form] = Form.useForm()
+  const emailRules = [
+    { required: true, message: '请输入' },
+    { type: 'email', message: '请输入合法的邮箱' }
+  ]
+  // const genderRules = [{ required: true, message: '请输入' }]
+  const companyRules = [{ required: true, message: '请输入' }]
+  const close = () => {
+    form.resetFields()
+    handleClose()
+  }
+  const submit = async () => {
+    const value = await form.validateFields()
+    if (value) {
+      const values = form.getFieldsValue()
+      const datas = { ...values }
+      datas.password = AES_ECB_ENCRYPT(datas.password, datas.email)
+      console.log('sadasda', datas)
+      if (!editStatus) await postUser(datas)
+      // if (editStatus) await putContractor(target.uuid, datas)
+      message.destroy()
+      message.success('操作成功')
+      handleOk(values)
+    }
+  }
+  const fetchData = async (target: any) => {
+    const { data } = await getContractorDetail(target.uuid)
+    return data
+  }
+  const init = async () => {
+    if (isEmpty(target)) {
+      setEditStatus(false)
+      form.setFieldsValue(new Contractor())
+      await setTitle('新增')
+    }
+    if (!isEmpty(target)) {
+      setEditStatus(true)
+      const data = await fetchData(target)
+      await setTitle('编辑')
+      form.setFieldsValue(data)
+    }
+  }
+  const handleUploadImg = () => {
+    const reader = new FileReader()
+    const f: any = document.getElementById('img')
+    f?.click()
+    f.onchange = async function (e: any) {
+      const file = e.target.files[0]
+      reader.readAsDataURL(file)
+      reader.onload = function () {
+        form.setFieldValue('picture', this.result)
+      }
+    }
+  }
+  useEffect(() => {
+    init()
+  }, [target])
+  return (
+    <Modal title={title} width={800} centered forceRender maskClosable={false} destroyOnClose={true} open={open} onOk={submit} onCancel={close}>
+      <Form id="form" form={form} labelCol={{ span: '4' }} layout="inline">
+        <Row>
+          <Col span={12}>
+            <Form.Item label="姓名" name="name" rules={companyRules}>
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="邮箱" name="email" rules={emailRules}>
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="部门" name="email" rules={emailRules}>
+              <Select>
+                <Select.Option value="sample">Sample</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="岗位" name="email" rules={emailRules}>
+              <Select>
+                <Select.Option value="sample">Sample</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="角色" name="email" rules={emailRules}>
+              <Select>
+                <Select.Option value="sample">Sample</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="密码" name="password">
+              <Input.Password />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        {/* <Form.Item label="性别" name="gender" rules={genderRules}>
+          <Radio.Group value={form.gender}>
+            <Radio value={0}>男</Radio>
+            <Radio value={1}>女</Radio>
+          </Radio.Group>
+        </Form.Item> */}
+      </Form>
+    </Modal>
+  )
+}
