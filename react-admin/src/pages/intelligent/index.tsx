acc4204b8582268700effb24b68b5f53a550e9d6
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-05-12 14:08:48 +0800

    添加智能体

---
+++ react-admin/src/pages/intelligent/index.tsx
@@ -0,0 +1,10 @@
+//import { Button } from "antd"
+//import useSystemStore from "@/store/index";
+import './index.scss'
+export default function Intelligent() {
+  return (
+    <section className='Intelligent'>
+        Intelligent
+    </section>
+  );
+}
