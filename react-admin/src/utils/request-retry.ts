b6b299252a2a3a38607a4f7ed352a5d3fb89eeec
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-05-22 20:55:33 +0800

    11

--- react-admin/src/utils/request-retry.ts
+++ react-admin/src/utils/request-retry.ts
@@ -1,4 +1,4 @@
-
-const resuest = (url, maxCount = 5) => {
-    return fetch(url).catch((err) => maxCount <= 0 ? Promise.reject(err) : resuest(url, maxCount - 1))
-}
+//
+//const resuest = (url, maxCount = 5) => {
+//    return fetch(url).catch((err) => maxCount <= 0 ? Promise.reject(err) : resuest(url, maxCount - 1))
+//}
