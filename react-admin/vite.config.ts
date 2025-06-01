dfc833e32f231153fbc1d69017d0cfddfe37e5af
wu-yiqiang <wu_yiqiang@outlook.com>
2025-06-01 14:03:06 +0800

    优化多环境配置

--- react-admin/vite.config.ts
+++ react-admin/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgLoader from 'vite-svg-loader'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
@@ -11,91 +11,95 @@ import { resolve } from 'path'
 const pathResolve = (dir: string): string => {
   return resolve(__dirname, '.', dir)
 }
-export default defineConfig({
-  envDir: './env',
-  base: './',
-  resolve: {
-    alias: {
-      '@': pathResolve('src/')
-      // components: pathResolve('src/components')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: `@import "@/style/mixin.scss";@import "@/style/global.scss";`
+
+export default defineConfig(({ command, mode }) => {
+  const envDir = './env'
+  const envConfig = loadEnv(mode, envDir)
+  return {
+    envDir: envDir,
+    base: './',
+    resolve: {
+      alias: {
+        '@': pathResolve('src/')
+        // components: pathResolve('src/components')
       }
-    }
-  },
-  plugins: [
-    imageOptimizer({ quality: 5 }),
-    basicSsl(),
-    react(),
-    svgLoader(),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [pathResolve('src/assets/icons/svg')],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]'
-    }),
-    buildCompress(),
-    visualizer({
-      gzipSize: true,
-      brotliSize: true,
-      emitFile: false,
-      filename: 'visualizer.html',
-      open: false
-    }),
-    autoUpload({
-      host: '185.198.166.245',
-      port: '3333',
-      username: 'root',
-      password: 'gUbP29RyGunM',
-      localPath: '/Users/atlas/react/react-admin/dist/',
-      remotePath: '/usr/share/nginx/html/easm-web/dist',
-    })
-  ],
-  server: {
-    host: '0.0.0.0',
-    https: false,
-    hmr: true,
-    port: 8090,
-    proxy: {
-      '/prod-api': {
-        target: `http://192.168.1.222:8000/`,
-        changeOrigin: true,
-        ws: false,
-        rewrite: (path) => path.replace(/^\/prod-api/, '')
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // javascriptEnabled: true,
+          additionalData: `@import "@/style/mixin.scss";@import "@/style/global.scss";`
+        }
       }
-    }
-  },
-  build: {
-    assetsDir: 'assets',
-    target: 'es2022',
-    polyfillModulePreload: true,
-    minify: 'esbuild',
-    cssCodeSplit: false,
-    cssTarget: '',
-    emptyOutDir: true,
-    reportCompressedSize: false,
-    chunkSizeWarningLimit: 500,
-    sourcemap: false,
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        format: 'esm',
-        manualChunks: {
-          echarts: ['echarts'],
-          vendor: ['react', 'react-dom', 'react-router-dom', 'lodash-es', 'dayjs', 'alova', 'zustand'],
-          antd: ['antd']
-        },
-        entryFileNames: 'js/[name]-[hash].js',
-        chunkFileNames: 'js/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          const fileName = assetInfo?.name
-          if (fileName?.endsWith('.css')) return 'css/[name]-[hash].css'
-          return 'assets/[ext]/[name]-[hash].[ext]'
+    },
+    plugins: [
+      imageOptimizer({ quality: 5 }),
+      basicSsl(),
+      react(),
+      svgLoader(),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [pathResolve('src/assets/icons/svg')],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]'
+      }),
+      buildCompress(),
+      visualizer({
+        gzipSize: true,
+        brotliSize: true,
+        emitFile: false,
+        filename: 'visualizer.html',
+        open: false
+      }),
+      autoUpload({
+        host: envConfig.VITE_HOST,
+        port: envConfig.VITE_PORT,
+        username: envConfig.VITE_USERNAME,
+        password: envConfig.VITE_PASSPWORD,
+        localPath: envConfig.VITE_LOCAL_PATH,
+        remotePath: envConfig.VITE_REMOTE_PATH
+      })
+    ],
+    server: {
+      host: '0.0.0.0',
+      hmr: true,
+      port: 8090,
+      proxy: {
+        '/prod-api': {
+          target: `http://192.168.1.222:8000/`,
+          changeOrigin: true,
+          ws: false,
+          rewrite: (path) => path.replace(/^\/prod-api/, '')
+        }
+      }
+    },
+    build: {
+      assetsDir: 'assets',
+      target: 'es2022',
+      polyfillModulePreload: true,
+      minify: 'esbuild',
+      cssCodeSplit: false,
+      cssTarget: '',
+      emptyOutDir: true,
+      reportCompressedSize: false,
+      chunkSizeWarningLimit: 500,
+      sourcemap: false,
+      outDir: 'dist',
+      rollupOptions: {
+        output: {
+          format: 'esm',
+          manualChunks: {
+            echarts: ['echarts'],
+            vendor: ['react', 'react-dom', 'react-router-dom', 'lodash-es', 'dayjs', 'alova', 'zustand'],
+            antd: ['antd']
+          },
+          entryFileNames: 'js/[name]-[hash].js',
+          chunkFileNames: 'js/[name]-[hash].js',
+          assetFileNames: (assetInfo) => {
+            const fileName = assetInfo?.name
+            if (fileName?.endsWith('.css')) return 'css/[name]-[hash].css'
+            return 'assets/[ext]/[name]-[hash].[ext]'
+          }
         }
       }
     }
