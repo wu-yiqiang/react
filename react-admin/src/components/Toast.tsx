ebba158f00e1e60840b87227c422d7707e6956d6
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-08-07 17:00:18 +0800

    优化Toast代码

--- react-admin/src/components/Toast.tsx
+++ react-admin/src/components/Toast.tsx
@@ -1,32 +1,31 @@
 import { message } from 'antd'
-import { JointContent } from 'antd/es/message/interface'
+import { JointContent, NoticeType } from "antd/es/message/interface";
 let messages: JointContent[] = []
 interface ToastInterface {
   info: (value: JointContent) => void;
   error: (value: JointContent) => void;
   success: (value: JointContent) => void;
 }
+const messageShow = (
+  messageType: NoticeType,
+  value: JointContent,
+  duration: number
+) => {
+  if (messages.includes(value)) return;
+  messages.push(value);
+  message[messageType](value, duration, () => {
+    messages = messages.filter((item) => item !== value);
+  });
+};
 const Toast: ToastInterface = {
   info: (value: JointContent, duration = 2) => {
-    if (messages.includes(value)) return
-    messages.push(value)
-    message.info(value, duration, () => {
-      messages = messages.filter((item) => item !== value)
-    })
+    messageShow('info',value, duration)
   },
   error: (value: JointContent, duration = 2) => {
-    if (messages.includes(value)) return
-    messages.push(value)
-    message.error(value, duration, () => {
-      messages = messages.filter((item) => item !== value)
-    })
+    messageShow("error", value, duration);
   },
   success: (value: JointContent, duration = 2) => {
-    if (messages.includes(value)) return
-    messages.push(value)
-    message.success(value, duration, () => {
-      messages = messages.filter((item) => item !== value)
-    })
+    messageShow("success", value, duration);
   }
 }
 
