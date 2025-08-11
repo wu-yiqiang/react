9bb2d37a0e420efeca110dbccea870805511c4c9
wu-yiqiang <wu_yiqiang@outlook.com>
2025-08-12 02:37:48 +0800

    添加sse功能

--- react-admin/src/pages/layout/head.tsx
+++ react-admin/src/pages/layout/head.tsx
@@ -3,12 +3,13 @@ import './head.scss'
 import { Input, Dropdown, MenuProps, Badge, Avatar } from 'antd'
 import eventMitt from "@/utils/eventMitt";
 import SettingDialog from './settings'
-import { useState } from 'react';
-import { logout } from '@/api/public';
-import Toast from '@/components/Toast';
-import useSystemStore from '@/store';
-import { SystemStore } from '@/types/common';
+import { useEffect, useState } from 'react'
+import { logout } from '@/api/public'
+import Toast from '@/components/Toast'
+import useSystemStore from '@/store'
+import { SystemStore } from '@/types/common'
 import { useTranslation } from 'react-i18next'
+import { sseService } from '@/network/sse'
 
 export default function Head() {
   return (
@@ -23,14 +24,14 @@ export default function Head() {
       <Setting />
       <User />
     </span>
-  );
+  )
 }
 
 function Search() {
   const { t } = useTranslation()
   return (
     <div className="search-expand-container">
-      <Input prefix={<SearchOutlined />} className="search-expanding-input"  placeholder={t('search')} />
+      <Input prefix={<SearchOutlined />} className="search-expanding-input" placeholder={t('search')} />
     </div>
   )
 }
@@ -47,35 +48,35 @@ const Lock = () => {
 
 function Translate() {
   const handleLanguage = (value: string) => {
-    eventMitt.emit("SYSTEM:LANGUAGE", value);
-  };
-  const items: MenuProps["items"] = [
+    eventMitt.emit('SYSTEM:LANGUAGE', value)
+  }
+  const items: MenuProps['items'] = [
     {
-      key: "en-US",
-      label: <a onClick={() => handleLanguage("en-US")}>English</a>,
+      key: 'en-US',
+      label: <a onClick={() => handleLanguage('en-US')}>English</a>
     },
     // {
     //   key: "AR",
     //   label: <a onClick={() => handleLanguage("AR")}>العربية</a>,
     // },
     {
-      key: "zh-CN",
-      label: <a onClick={() => handleLanguage("zh-CN")}>简体中文</a>,
-    },
-  ];
+      key: 'zh-CN',
+      label: <a onClick={() => handleLanguage('zh-CN')}>简体中文</a>
+    }
+  ]
   return (
     <Dropdown menu={{ items }} placement="bottom" arrow>
       <TranslationOutlined className="headeIcon" />
     </Dropdown>
-  );
+  )
 }
 
 function Theme() {
   const { t } = useTranslation()
   const handleTheme = (value: string) => {
-    eventMitt.emit("SYSTEM:THEME", value);
+    eventMitt.emit('SYSTEM:THEME', value)
     // EventMitt("changeTheme", value);
-  };
+  }
   const items: MenuProps['items'] = [
     {
       key: 'light',
@@ -94,12 +95,25 @@ function Theme() {
     <Dropdown menu={{ items }} placement="bottom" arrow>
       <BgColorsOutlined className="headeIcon" />
     </Dropdown>
-  );
+  )
 }
 
 function Notion() {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const params = {
+      url: 'http://127.0.0.1:7788/sse',
+      onmessage: ({ data }: any) => {
+        const datas = JSON.parse(data)
+        datas ? setCount(datas) : setCount(0)
+      },
+      onopen: () => {},
+      finallyHandler: () => {}
+    }
+    sseService.connect(params)
+  }, [])
   return (
-    <Badge count={100} offset={[-3, 0]}>
+    <Badge count={count} offset={[-3, 0]}>
       <BellOutlined className="headeIcon" />
     </Badge>
   )
