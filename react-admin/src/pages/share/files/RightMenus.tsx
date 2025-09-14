74fbf8ca1022d9d7dbf73411e5172a5b3a868dea
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-09-14 16:47:21 +0800

    添加右键菜单组件

---
+++ react-admin/src/pages/share/files/RightMenus.tsx
@@ -0,0 +1,10 @@
+// import { Button, Modal, Flex, Space } from "antd";
+// import { useMemo, useState } from "react";
+export default function RightMenu(props: any) {
+  // const { selectedLists } = props;
+  return (
+    <>
+      右键菜单
+    </>
+  );
+}
